b8fe4330d8d02403c951d3e61729bb475530f9d6
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-03-13 20:39:01 +0400

    started experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import "./Experience.scss";
+import Skills from "./skills/Skills";
 function Experience() {
-  return <div className="experience">Experience</div>;
+  return (
+    <section id="#experience">
+      <div className="headings">
+        <h5>what skills I posses</h5>
+        <h2>My Experience</h2>
+      </div>
+      <p className="quote">
+        “There's always room for improvement no matter what.” -{" "}
+        <span className="ali">Ali Krieger</span>
+      </p>
+      <div className="skills">
+        <Skills />
+      </div>
+    </section>
+  );
 }
 
 export default Experience;
