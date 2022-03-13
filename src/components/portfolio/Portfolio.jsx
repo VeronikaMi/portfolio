b6eade793b588ba66af3cf46744a89939f617cb6
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-03-13 22:13:45 +0400

    added portfolio section , need to work on responsiveness , also fixed media in home section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,38 @@
 import React from "react";
+import "./Portfolio.scss";
 function Portfolio() {
-  return <div className="portfolio">Portfolio</div>;
+  const projects = [
+    { id: 1, name: "Ableton Clone", src: "" },
+    { id: 2, name: "Ableton Clone", src: "" },
+    { id: 3, name: "Ableton Clone", src: "" },
+  ];
+  return (
+    <section id="portfolio">
+      <div className="headings">
+        <h5>projects I worked on</h5>
+        <h2>My Portfolio</h2>
+      </div>
+      <div className="projects">
+        {projects.map((project) => (
+          <div className="project" key={project.id}>
+            <div className="img"></div>
+            <h3>{project.name}</h3>
+            <div className="btn-container">
+              <button type="button" className="btn btn-primary">
+                See Live
+              </button>
+              <button type="button" className="btn btn-secondary">
+                GitHub
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button type="button" className="btn btn-primary see-more">
+        See More
+      </button>
+    </section>
+  );
 }
 
 export default Portfolio;
