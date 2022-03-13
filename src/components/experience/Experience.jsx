9a47418abd70096fbe807f9d29ea88a4c788ec31
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-03-13 21:26:08 +0400

    experience section done for now

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Experience.scss";
-import Skills from "./skills/Skills";
 function Experience() {
+  const skills = [
+    { id: 1, skill: "HTML5", progress: "80%" },
+    { id: 2, skill: "CSS3", progress: "80%" },
+    { id: 3, skill: "SASS", progress: "80%" },
+    { id: 4, skill: "JavaScript", progress: "80%" },
+    { id: 5, skill: "TypeScript", progress: "80%" },
+    { id: 6, skill: "Angular", progress: "80%" },
+    { id: 7, skill: "React", progress: "80%" },
+  ];
+
   return (
     <section id="#experience">
       <div className="headings">
@@ -13,8 +22,16 @@ function Experience() {
         <span className="ali">Ali Krieger</span>
       </p>
       <div className="skills">
-        <Skills />
+        {skills.map((skill) => (
+          <div className="skill" key={skill.id}>
+            <h4>{skill.skill}</h4>
+            <div className="progress-bar">
+              <div className="progress" style={{ width: skill.progress }}></div>
+            </div>
+          </div>
+        ))}
       </div>
+      <p className="growth">...and growing</p>
     </section>
   );
 }
