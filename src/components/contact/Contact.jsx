1f388dd8adba8faa5fa8b496a78fb8df5bd040bb
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-03-14 22:09:48 +0400

    contact section done just html+css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,53 @@
 import React from "react";
+import "./Contact.scss";
+import { GoMail } from "react-icons/go";
+import { RiMessengerLine } from "react-icons/ri";
+
 function Contact() {
-  return <div className="contact">Contact</div>;
+  const sendMessage = (form) => {
+    console.log(form);
+  };
+  return (
+    <section id="contact">
+      <div className="headings">
+        <h5>let’s talk</h5>
+        <h2>Contact Me</h2>
+      </div>
+      <div className="contact-container">
+        <form onSubmit={sendMessage}>
+          <div className="input-field">
+            <label htmlFor="fullName">Full Name</label>
+            <input type="text" name="fullName" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="mail">Email</label>
+            <input type="email" name="mail" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+        <div className="triggers">
+          <div className="trigger">
+            <GoMail size={33} />
+            <h4>Email</h4>
+            <h6>myeamil@gmail.com</h6>
+            <a href="mailto:">Send a message</a>
+          </div>
+          <div className="trigger">
+            <RiMessengerLine size={33} />
+            <h4>Messenger</h4>
+            <h6>facebookid</h6>
+            <a href="https/m.me/">Send a message</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
