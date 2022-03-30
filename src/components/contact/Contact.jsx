259e450071e6277cc7ad595e345517978957d86d
VeronikaMi <nika.mitichashili@gmail.com>
2022-03-30 16:13:35 +0400

    got the contact section working

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./Contact.scss";
 import { GoMail } from "react-icons/go";
 import { RiMessengerLine } from "react-icons/ri";
+import emailjs from "emailjs-com";
 
 function Contact() {
-  const sendMessage = (form) => {
-    console.log(form);
+  const form = useRef();
+  const sendMessage = (e) => {
+    e.preventDefault(); //doesn't send the form on default 
+    emailjs.sendForm('service_04z8af9', 'template_bx26nh7',form.current,'gU0vNx5Gn9Tf4FTCD');
+    e.target.reset(); //resets the form
   };
   return (
     <section id="contact">
@@ -14,7 +18,7 @@ function Contact() {
         <h2>Contact Me</h2>
       </div>
       <div className="contact-container">
-        <form onSubmit={sendMessage}>
+        <form ref={form} onSubmit={sendMessage}>
           <div className="input-field">
             <label htmlFor="fullName">Full Name</label>
             <input type="text" name="fullName" />
@@ -36,13 +40,13 @@ function Contact() {
             <GoMail size={33} />
             <h4>Email</h4>
             <h6>myeamil@gmail.com</h6>
-            <a href="mailto:">Send a message</a>
+            <a href="mailto:nika.mitichashili@gmail.com" target="_blank">Send a message</a>
           </div>
           <div className="trigger">
             <RiMessengerLine size={33} />
             <h4>Messenger</h4>
             <h6>facebookid</h6>
-            <a href="https/m.me/">Send a message</a>
+            <a href="https://m.me/VeronikaMi01" target="_blank">Send a message</a>
           </div>
         </div>
       </div>
