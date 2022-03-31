5d02766a2026dc77148a5a7d63e3167537d8287b
VeronikaMi <nika.mitichashili@gmail.com>
2022-03-31 12:04:49 +0400

    fixed nav with z-index , disappears behind footer

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import About from "./about/About";
 import Contact from "./contact/Contact";
 import Experience from "./experience/Experience";
@@ -6,39 +6,9 @@ import Footer from "./footer/Footer";
 import Home from "./home/Home";
 import Portfolio from "./portfolio/Portfolio";
 import Nav from "./nav/Nav";
-// import { useEffect } from "react";
-
 function HomePage() {
-  // let height = document.documentElement.scrollHeight;
-  // useEffect(()=>{
-  //   if(document.documentElement.scrollHeight - document.documentElement.scrollTop <= 500){
-  //     console.log("hide nav")
-  //   }
-  // })
-
-  let container = useRef();
-  const [scrolledHeight, setScrolledHeight] = useState(container.current.scrollTop);
-
-  const handleScroll = (e)=>{
-    console.log("Scrolled")
-    
-    console.log(e)
-    // onScroll = {handleScroll}
-  }
-
-  useEffect(()=>{
-    console.log(container)
-    const { scrollTop, scrollHeight, clientHeight } = container.current;
-    console.log(scrollTop, scrollHeight, clientHeight);
-      if (clientHeight - scrollTop === 200) {
-        console.log("reached bottom");
-      }
-  },[scrolledHeight])
-
   return (
     <>
-    <div ref={container}
-    onScroll={()=>setScrolledHeight(container.current.scrollTop)}>
       <Nav/>
       <Home />
       <About />
@@ -46,7 +16,6 @@ function HomePage() {
       <Portfolio />
       <Contact />
       <Footer />
-    </div>
     </>
   );
 }
